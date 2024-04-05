7f48435b9957556ad6d54ad18d0f782475d4cf5a
Raneemq1 <raneemalqadi32@gmail.com>
2024-04-05 23:36:20 +0300

    add bot observers

--- Program.cs
+++ Program.cs
@@ -9,10 +9,13 @@ namespace WeatherMonitoringSystem
         static void Main(string[] args)
         {
             Console.WriteLine("Weather Monitoring System");
-            var bots = FileConfig();
-            
-      
 
+            var bots = FileConfig();
+
+            IWeatherSubject subject = new WeatherSubject();
+            AddBotObserversTOSubject(subject,bots);
+
+           
         }
 
         public static IEnumerable<Bot> FileConfig()
@@ -21,7 +24,17 @@ namespace WeatherMonitoringSystem
 
             return configFileReader.Read(FilePath.filePath);
         }
+        public static void AddBotObserversTOSubject(IWeatherSubject subject ,IEnumerable<Bot> bots)
+        {
+            RainBot rainBot = (RainBot)bots.FirstOrDefault(bot => bot.GetType().Name is "RainBot");
+            SnowBot snowBot = (SnowBot)bots.FirstOrDefault(bot => bot.GetType().Name is "SnowBot");
+            SunBot sunBot = (SunBot)bots.FirstOrDefault(bot => bot.GetType().Name is "SunBot");
+            subject.AddObserver(snowBot);
+            subject.AddObserver(sunBot);
+            subject.AddObserver(rainBot);
+        }
 
        
+
     }
 }
